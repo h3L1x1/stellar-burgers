a4ec9fd6131e9a936b295029c636757c1810d5d8
h3L1x1 <zikov.sasha666@yandex.ru>
2025-12-04 09:41:21 +0300

    jest rootReducer fix

--- src/services/slices/tests/store.test.ts
+++ src/services/slices/tests/store.test.ts
@@ -1,20 +1,34 @@
 import store, { rootReducer } from '../../store';
+import ingredientsSlice from '../../slices/ingredientsSlice';
+import orderSlice from '../../slices/orderSlice';
+import feedSlice from '../../slices/feedSlice';
+import burgerConstructorSlice from '../../slices/burgerConstructorSlice';
+import authSlice from '../../slices/authSlice';
+import userOrdersSlice from '../../slices/userOrderSlice';
 
 describe('rootReducer', () => {
+  const initAction = { type: '@@INIT' };
+
   it('should be defined', () => {
     expect(rootReducer).toBeDefined();
   });
 
-  it('slices test', () => {
-    const initialState = rootReducer(undefined, { type: '@@INIT' });
+  it('rootReducer должен возвращать корректное начальное состояние', () => {
+    const state = rootReducer(undefined, initAction);
 
-    expect(initialState).toEqual({
-      ingredients: expect.any(Object),
-      order: expect.any(Object),
-      feed: expect.any(Object),
-      burgerConstructor: expect.any(Object),
-      auth: expect.any(Object),
-      userOrders: expect.any(Object)
+    expect(state).toEqual({
+      ingredients: ingredientsSlice(undefined, initAction),
+      order: orderSlice(undefined, initAction),
+      feed: feedSlice(undefined, initAction),
+      burgerConstructor: burgerConstructorSlice(undefined, initAction),
+      auth: authSlice(undefined, initAction),
+      userOrders: userOrdersSlice(undefined, initAction)
     });
+  });
+
+  it('rootReducer должен возвращать то же состояние при неизвестном экшене', () => {
+    const prevState = store.getState();
+    const state = rootReducer(prevState, { type: 'UNKNOWN_ACTION' });
+    expect(state).toBe(prevState);
   });
 });
